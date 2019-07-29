0561bbaa82576bc1f66ce94b5ca810edac38f400
Samuel Martinucci <samuel.martinucci@aurea.com>
2019-07-29 08:19:19 -0700

    Initial import

---
+++ src/main/java/test/test/testing/C.java
@@ -0,0 +1,14 @@
+package test.test.testing;
+
+public class C {
+    public void x(String test) {
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
+        System.out.println("6");
+        System.out.println("7");
+    }
+}
+
